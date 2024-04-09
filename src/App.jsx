9f74449b30f45bf1e26e1e0587a057335d9d4a63
Carson Ruscin <carson.xlive@gmail.com>
2024-04-09 15:40:01 -0500

    Part One Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css"
 export const App = () => {
   const [allTickets, setAllTickets] = useState([])
   const [showEmergency, setShowEmergency] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
 
   useEffect(() => {
     getAllTickets().then(ticketsArray => {
@@ -13,14 +14,24 @@ export const App = () => {
     })
   }, []) // with an empty dependency array, it ONLY runs on initial render of the component **
 
+  useEffect(() => {
+    if(showEmergency === true) {
+      const emergencyTickets = allTickets.filter(ticket => ticket.emergency === true)
+      setFilteredTickets(emergencyTickets)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergency, allTickets])
+
   return (
     <div className="tickets-container">
       <h2>Tickets</h2>
       <div>
         <button className="filter-btn btn-primary" onClick={() => {setShowEmergency(true)}}>Emergency</button>
+        <button className="filter-btn btn-info" onClick={() => {setShowEmergency(false)}}>Show All</button>
       </div>
       <article className="tickets">
-        {allTickets.map((ticket) => {
+        {filteredTickets.map((ticket) => {
           return (
             <section className="ticket" key={ticket.id}>
               <header className="ticket-info">#{ticket.id}</header>
